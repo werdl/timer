3337a2ece848df50242714cac2dbf8d4271d3d43
werdl <werdl_is_cool@outlook.com>
2023-12-23 08:00:37 +0000

    add better dark mode, keybind explainers etc

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import time
 
 app = Flask(__name__)
 
-@app.route('/timer/<int:seconds>')
+@app.route('/<int:seconds>')
 def timer(seconds):
     return render_template('timer.html', seconds=seconds)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/<int:minutes>:<int:seconds>')
+def mtimer(minutes, seconds):
+    return render_template('timer.html', seconds=(minutes * 60 ) + seconds)
+
+@app.route('/<int:hours>:<int:minutes>:<int:seconds>')
+def htimer(hours, minutes, seconds):
+    return render_template('timer.html', seconds=(hours * 3600) + (minutes * 60 ) + seconds)
+
+@app.route('/')
+def home():
+    return redirect('/1:00')
